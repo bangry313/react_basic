38f6cc028895260d34e33e52328b4de552427075
bangry313 <bangry313@gmail.com>
2024-04-25 17:38:42 +0900

    useRef 훅 사용 완료

--- src/componens/ArticleList.js
+++ src/componens/ArticleList.js
@@ -1,15 +1,41 @@
 import { useEffect, useRef, useState } from "react";
-import httpRequest from "../http";
+
+const httpRequest = function(url){
+  return fetch(url).then((response)=> response.json() );
+}
 
 const ArticleList = function () {
-  const [articles, setAticles] = useState([
-    { id: 1, title: '점심먹으러 갑시다', writer: '가을님1' },
-    { id: 2, title: '점심먹으러 갑시다', writer: '가을님2' },
-    { id: 3, title: '점심먹으러 갑시다', writer: '가을님3' },
-    { id: 4, title: '점심먹으러 갑시다', writer: '가을님4' },
-    { id: 5, title: '점심먹으러 갑시다', writer: '가을님5' },
-  ]);
+  
+  const getPosts = async function(){
+    const uri = "https://jsonplaceholder.typicode.com/posts";
+    const posts =  await httpRequest(uri);
+    setAticles(posts);
+    // console.log(posts);
+  }
+
+  const [articles, setAticles] = useState(
+    []
+  //   [
+  //   { id: 1, title: '점심먹으러 갑시다', writer: '가을님1' },
+  //   { id: 2, title: '점심먹으러 갑시다', writer: '가을님2' },
+  //   { id: 3, title: '점심먹으러 갑시다', writer: '가을님3' },
+  //   { id: 4, title: '점심먹으러 갑시다', writer: '가을님4' },
+  //   { id: 5, title: '점심먹으러 갑시다', writer: '가을님5' },
+  // ]
+);
   const [title, setTitle] = useState("");
+
+  useEffect(()=>{
+    getPosts();
+    // 언마운트될 때 수행하는 메소드 반환
+    return ()=>{
+      console.log("나 화면에서 지워집니다...");
+    }
+
+  }, []);
+
+  const articleInput = useRef(null);
+
 
   const articleList = articles.map(article => {
     return <tr key={article.id}>
@@ -31,6 +57,7 @@ const ArticleList = function () {
     const newArticles = [...articles, newArticle];
     setAticles(newArticles);
     setTitle("");
+    articleInput.current.focus();
   }
 
   // 게시글 삭제 이벤트 처리
@@ -43,7 +70,7 @@ const ArticleList = function () {
     <>
       <h2>게시글 목록</h2>
 
-      <input type="text" value={title} onChange={handleTitleChange}/>
+      <input type="text" value={title} onChange={handleTitleChange} ref={articleInput}/>
       <button onClick={handleArticleAdd}>게시글 등록</button>
 
       <table border="1">
